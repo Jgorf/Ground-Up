1a1c9fb07bb5ebf3c6b2694fe644e71d81a39934
Jeremy Gorfinkel <gorfinkeljc@gmail.com>
2022-08-08 16:40:04 -0400

    added link to javascript
    added an obfuscated mailto link to the email in footer

---
+++ spamProof.js
@@ -0,0 +1,3 @@
+let emailPar = document.getElementById('email');
+let finishedEmail = 'hello'+'@'+'groundup'+'.'+'com';
+emailPar.innerHTML = '<a ' + 'style="color:#6B350E;"' + 'hre' + 'f="mai' + 'lto:' + finishedEmail + '">' + finishedEmail + '</' + 'a>';
