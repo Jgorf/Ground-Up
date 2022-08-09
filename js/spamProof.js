24987080b2ba4196b03ea553171a630a5bf072c9
Jeremy Gorfinkel <gorfinkeljc@gmail.com>
2022-08-08 21:12:23 -0400

    added hover enter and exit events for the email in footer

--- js/spamProof.js
+++ js/spamProof.js
@@ -1,3 +1,12 @@
 let emailPar = document.getElementById('email');
 let finishedEmail = 'hello'+'@'+'groundup'+'.'+'com';
 emailPar.innerHTML = '<a ' + 'style="color:#6B350E;"' + 'hre' + 'f="mai' + 'lto:' + finishedEmail + '">' + finishedEmail + '</' + 'a>';
+emailPar.addEventListener('mouseover', hoverEnter);
+emailPar.addEventListener('mouseout', hoverLeave);
+
+function hoverEnter() {
+    emailPar.childNodes[0].style.color = '#efc143';
+}
+function hoverLeave() {
+    emailPar.childNodes[0].style.color = '#6B350E';
+}
