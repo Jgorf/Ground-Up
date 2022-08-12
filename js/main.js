f7973facd33edfb67292a102e5ee78cf05e79aab
Jeremy Gorfinkel <gorfinkeljc@gmail.com>
2022-08-11 23:18:18 -0400

    added javascript to toggle a class on and off when the hamburger menu div is clicked

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ let emailPar = document.getElementById('email');
 let finishedEmail = 'hello'+'@'+'groundup'+'.'+'com';
 let steamPieces = document.getElementById('steam').childNodes;
 let groundupLogo = document.getElementById('header-logo');
+let menuDiv = document.querySelector('header > div');
 emailPar.innerHTML = '<a ' + 'style="color:#6B350E;"' + 'hre' + 'f="mai' + 'lto:' + finishedEmail + '">' + finishedEmail + '</' + 'a>';
 emailPar.addEventListener('mouseenter', hoverEnter);
 emailPar.addEventListener('mouseleave', hoverLeave);
 groundupLogo.addEventListener('mouseenter', floatUp);
-groundupLogo.addEventListener('mouseleave', floatReset);
+groundupLogo.addEventListener('mouseleave', floatReset)
+menuDiv.addEventListener('click', toggleNav);
 let tl = new gsap.timeline();   //{onComplete: floatReset} works but takes too long for me to use want to use it
 function hoverEnter() {
     emailPar.childNodes[0].style.color = '#efc143';
@@ -24,6 +26,9 @@ function floatUp() {
 function floatReset() {
     tl.restart();
     tl.pause();
+}
+function toggleNav() {
+	menuDiv.classList.toggle('openNav');
 }
 /* How do I reset timeline at the end of animation? */
 /* Why don't I need to use import { gsap } from "gsap"; */
