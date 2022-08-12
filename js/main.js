6826a123c22042d9b2dc5d38b6d4931b5cf37d7c
Jeremy Gorfinkel <gorfinkeljc@gmail.com>
2022-08-12 16:51:20 -0400

    fixed steam animation
    added code to play sliding animation for the nav menu on mobile devices
    
    added a staggering opacity to elements in nav menu

--- js/main.js
+++ js/main.js
@@ -2,14 +2,25 @@ let emailPar = document.getElementById('email');
 let finishedEmail = 'hello'+'@'+'groundup'+'.'+'com';
 let steamPieces = document.getElementById('steam').childNodes;
 let groundupLogo = document.getElementById('header-logo');
-let menuDiv = document.querySelector('header > div');
+
+let menuWrapper = document.querySelector('header > div');
+let menu = document.querySelector('header nav ul');
+let menuItems = document.querySelectorAll('header nav li');
+let isOpen = false; //nav default to closed
+
 emailPar.innerHTML = '<a ' + 'style="color:#6B350E;"' + 'hre' + 'f="mai' + 'lto:' + finishedEmail + '">' + finishedEmail + '</' + 'a>';
 emailPar.addEventListener('mouseenter', hoverEnter);
 emailPar.addEventListener('mouseleave', hoverLeave);
 groundupLogo.addEventListener('mouseenter', floatUp);
-groundupLogo.addEventListener('mouseleave', floatReset)
-menuDiv.addEventListener('click', toggleNav);
-let tl = new gsap.timeline();   //{onComplete: floatReset} works but takes too long for me to use want to use it
+
+menuWrapper.addEventListener('click', gsapToggle);
+
+let tlSteam = new gsap.timeline();
+let tlMenu = new gsap.timeline();
+tlMenu.pause();
+tlMenu.to('header nav ul', {x:'-101%', duration:.5});
+tlMenu.fromTo('header nav li', {opacity:0}, {opacity:1, duration:.35, stagger:.15}, '-=.25'); 
+
 function hoverEnter() {
     emailPar.childNodes[0].style.color = '#efc143';
 }
@@ -17,18 +28,21 @@ function hoverLeave() {
     emailPar.childNodes[0].style.color = '#6B350E';
 }
 function floatUp() {
-	tl.to('#steam path', {duration:3, y:-20, stagger:.125});
-	tl.set('#steam path', {y:0}, '+=0.5');
-	tl.play();
-	//tl.then(floatReset); //works how I want the below line to work but only works once
-     //this resets it but repeats more than once when I dont want it to
+	if (tlSteam.progress() == 1 || tlSteam.progress() == 0) {
+		tlSteam.to('#steam path', {duration:3, y:-20, stagger:.125});
+		tlSteam.set('#steam path', {y:0}, '+=0.5');
+	}
+	return;
 }
-function floatReset() {
-    tl.restart();
-    tl.pause();
+function gsapToggle() {
+	
+	if (isOpen == false) {
+		isOpen = true;
+		tlMenu.timeScale(1).play();
+	}
+	else {
+		isOpen = false;
+		
+		tlMenu.pause().timeScale(2).reverse();
+	}
 }
-function toggleNav() {
-	menuDiv.classList.toggle('openNav');
-}
-/* How do I reset timeline at the end of animation? */
-/* Why don't I need to use import { gsap } from "gsap"; */
